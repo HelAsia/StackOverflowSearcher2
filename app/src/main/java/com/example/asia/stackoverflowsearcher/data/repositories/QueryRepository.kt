58df72d903547d235d19ff706d012413f93d0f37
HelAsia <asiaheluszka1993@gmail.com>
2018-12-09 00:12:12 +0100

    QueryRepositoryInterface was created

---
+++ app/src/main/java/com/example/asia/stackoverflowsearcher/data/repositories/QueryRepository.kt
@@ -0,0 +1,4 @@
+package com.example.asia.stackoverflowsearcher.data.repositories
+
+class QueryRepository {
+}
---
+++ app/src/main/java/com/example/asia/stackoverflowsearcher/data/repositories/QueryRepositoryInterface.kt
@@ -0,0 +1,12 @@
+package com.example.asia.stackoverflowsearcher.data.repositories
+
+import com.example.asia.stackoverflowsearcher.data.model.Item
+
+interface QueryRepositoryInterface {
+    interface OnQueryResultDisplayListener{
+        fun onSuccess(itemList: List<Item>?)
+        fun onError(errorMessageText: String?)
+    }
+
+    fun getQueryResult(title: String?, listener: OnQueryResultDisplayListener?)
+}
